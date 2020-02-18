7fb5107109b81548ad966f9dce3f4269330df541
hkayaoguzhan <oguzhanharmankayaa@gmail.com>
2020-02-18 16:16:19 +0300

    newUser

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,13 +1,26 @@
-app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
-    
-    const connectionOptions = {
+app.controller("indexController", [
+  "$scope",
+  "indexFactory",
+  ($scope, indexFactory) => {
+    $scope.init = () => {
+      const username = prompt("Please enter username");
+
+      if (username) 
+        initSocket(username);
+      else 
+        return false;
+    };
+    function initSocket(username) {
+      const connectionOptions = {
         reconnectionAttemts: 3,
         reconnectionDelay: 600
+      };
+      indexFactory.connectSocket("http://localhost:3000/", connectionOptions)
+        .then(socket => {
+          socket.emit('newUser', { username });
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
-    indexFactory.connectSocket('http://localhost:3000/', connectionOptions)  
-    .then((socket) => {
-        console.log("bağlandı", socket)
-    }).catch((err) => {
-        console.log(err);
-    })
 }]);
